0d307ea2a19f4c756121d011f0ff58790a3e20f8
Rahim Siddiqui <rahimsiddiqui122@gmail.com>
2026-04-21 13:04:05 +0500

    Added redirect to /dashboard

--- app/auth/page.jsx
+++ app/auth/page.jsx
@@ -1,7 +1,16 @@
 import AuthClient from "./AuthClient";
 import { Suspense } from "react";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("token")?.value;
+
+  if (token) {
+    redirect("/dashboard");
+  }
+
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <AuthClient />
